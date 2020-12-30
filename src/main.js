63b4ddaf655c4b64907a47b5aa5c05bc24157c92
nsmart <nsmart2421999@gmail.com>
2020-12-30 18:33:23 +0700

    Setup BoostrapVue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './styles/index.scss'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
